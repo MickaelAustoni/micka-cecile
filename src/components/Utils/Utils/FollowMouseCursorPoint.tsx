3ebceb1c0859650fcf6f3db8ef35f8a0b629cf97
Mibiki <108873902+MickaelAustoni@users.noreply.github.com>
2024-11-19 00:14:04 +0100

    feat: cursor animation

--- src/components/Utils/Utils/FollowMouseCursorPoint.tsx
+++ src/components/Utils/Utils/FollowMouseCursorPoint.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { CSSProperties } from "react";
+import { CSSProperties, useEffect, useRef } from "react";
 import { cubicBezier, motion, useScroll } from "framer-motion";
 import useFollowPointer from "@/hooks/useFollowPointer";
 import useIsTouchDevice from "@/hooks/useIsTouchDevice";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import heartAnimation from "@/assets/animations/heart.json";
 
 export interface FollowMouseCursorPointProps {
   size?: number;
@@ -13,41 +15,68 @@ export interface FollowMouseCursorPointProps {
 
 const styles: CSSProperties = {
   borderRadius: "50%",
-  left: -10,
   mixBlendMode: "difference",
   pointerEvents: "none",
   position: "fixed",
-  top: -10,
   userSelect: "none",
   zIndex: 10000,
 };
 
 const FollowMouseCursorPoint = ({
-  size = 25,
-  color = "#fff",
+  size = 150,
   opacity = 0.8,
 }: FollowMouseCursorPointProps) => {
   const { scrollY} = useScroll();
   const { x, y} = useFollowPointer();
-  const isTouchDevice = useIsTouchDevice()
+  const isTouchDevice = useIsTouchDevice();
+  const heartAnimationRef = useRef<LottieRefCurrentProps>(null);
 
-  if (isTouchDevice){
+  useEffect(() => {
+    const handleClick = () => {
+      heartAnimationRef?.current?.goToAndPlay(50, true);
+    };
+
+    // listen for clicks on the heart animation
+    window.addEventListener("click", handleClick);
+
+    return () => {
+      window.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  if (isTouchDevice) {
     return null
   }
 
-  return <motion.div
-    animate={{ x, y:  y- scrollY.get() }}
-    transition={{
-      duration: 0.1,
-      ease: cubicBezier(0.18, 0.89, 0.32, 1.28)
-    }}
-    style={{
-      ...styles,
-      backgroundColor: color,
-      height: `${size}px`,
-      opacity,
-      width: `${size}px`,
-    }} />;
+  return (
+    <motion.div
+      animate={{x, y: y - scrollY.get()}}
+      transition={{
+        duration: 0.1,
+        ease: cubicBezier(0.18, 0.89, 0.32, 1.28)
+      }}
+      style={{
+        ...styles,
+        height: `${size}px`,
+        opacity,
+        width: `${size}px`,
+        pointerEvents: "none",
+        left:  -(size / 2),
+        top: -(size / 2),
+      }}>
+      <Lottie
+        lottieRef={heartAnimationRef}
+        key="heartAnimation"
+        animationData={heartAnimation}
+        loop={false}
+        autoplay={false}
+        style={{ pointerEvents: "none" }}
+        onComplete={() => {
+          heartAnimationRef?.current?.goToAndStop(0, true);
+        }}
+      />
+    </motion.div>
+  );
 };
 
 export default FollowMouseCursorPoint;
