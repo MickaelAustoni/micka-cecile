868cd3ccf710516cd048bc3eed278f025cc71882
Mibiki <108873902+MickaelAustoni@users.noreply.github.com>
2024-11-23 22:21:40 +0100

    feat: improvement

--- src/app/components/InvitationForm.tsx
+++ src/app/components/InvitationForm.tsx
@@ -4,45 +4,49 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 interface InvitationFormProps {
   play?: boolean;
+  delay?: number;
 }
 
-export default function InvitationForm({ play = false }: InvitationFormProps) {
+export default function InvitationForm({ delay, play = false }: InvitationFormProps) {
   return (
     <AnimatePresence>
       {play && (
-        <motion.div
-          className="inset-0 z-[60] overflow-hidden absolute justify-center items-center flex bg-white"
-          initial={{
-            y: "100%",
-            rotateX: 45,
-            scale: 0.9,
-            opacity: 0,
-            transformPerspective: 2000,
-            transformOrigin: "center bottom"
-          }}
-          animate={{
-            y: 0,
-            rotateX: 0,
-            scale: 1,
-            opacity: 1
-          }}
-          exit={{
-            y: "100%",
-            rotateX: 45,
-            scale: 0.9,
-            opacity: 0
-          }}
-          transition={{
-            type: "spring",
-            damping: 25,
-            stiffness: 80,
-            duration: 1,
-          }}
-        >
-          <div className="container">
-            FORM HERE
-          </div>
-        </motion.div>
+        <div className="absolute inset-0 z-[60] overflow-hidden">
+          <motion.div
+            className="absolute inset-0 justify-center items-center flex bg-white"
+            initial={{
+              y: "100%",
+              rotateX: 45,
+              scale: 0.9,
+              opacity: 0,
+              transformPerspective: 2000,
+              transformOrigin: "center bottom"
+            }}
+            animate={{
+              y: 0,
+              rotateX: 0,
+              scale: 1,
+              opacity: 1
+            }}
+            exit={{
+              y: "100%",
+              rotateX: 45,
+              scale: 0.9,
+              opacity: 0
+            }}
+            transition={{
+              type: "spring",
+              damping: 25,
+              stiffness: 80,
+              duration: 1,
+              delay
+            }}
+          >
+            <div className="container">
+              FORM HERE
+            </div>
+          </motion.div>
+        </div>
       )}
     </AnimatePresence>
   );
