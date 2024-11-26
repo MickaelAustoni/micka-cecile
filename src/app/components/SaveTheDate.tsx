edfc4620bb16a2101f5ef882b06fb9a498139e01
Mibiki <108873902+MickaelAustoni@users.noreply.github.com>
2024-11-26 22:58:20 +0100

    feat: improvement save the date responsive

--- src/app/components/SaveTheDate.tsx
+++ src/app/components/SaveTheDate.tsx
@@ -37,106 +37,103 @@ export default function SaveTheDate({ delay, play = false }: InvitationFormProps
   return (
     <AnimatePresence>
       {play && (
-        <section className="absolute inset-0 z-[60] overflow-hidden">
+        <motion.section
+          className="absolute inset-0 justify-center items-center flex bg-brown z-[60] p-6 overflow-hidden"
+          transition={{
+            duration: 1,
+            delay
+          }}
+        >
+          {/* Logo */}
           <motion.div
-            className="absolute inset-0 justify-center items-center flex bg-brown"
+            initial={{
+              opacity: 0
+            }}
+            animate={{
+              opacity: 1
+            }}
             transition={{
               duration: 1,
-              delay
+              delay: 7.5
             }}
           >
-            {/* Logo */}
+            <Logo
+              disableHeartEnding
+              disableAnimation
+              shapeOnly
+              className="absolute left-1/2 top-6 -translate-x-1/2 max-w-xs"
+            />
+          </motion.div>
+
+          {/* Title */}
+          <div className="container z-10">
+            <motion.h1
+              className="text-center text-white drop-shadow-xl overflow-hidden text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl"
+              initial={{opacity: 0}}
+              animate={{opacity: 1}}
+              transition={{
+                duration: 2,
+                delay: delay ? delay + DELAY_TITLE : DELAY_TITLE,
+              }}
+            >
+              <motion.span
+                initial={{y: 100}}
+                animate={{y: 0}}
+                transition={{
+                  duration: 2.5,
+                  delay: delay ? delay + DELAY_TITLE : DELAY_TITLE,
+                  ease: [0.6, 0.01, 0.05, 0.95]
+                }}
+                style={{display: 'inline-block'}}
+              >
+                Save the date
+              </motion.span>
+            </motion.h1>
             <motion.div
+              className="text-center text-white text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl drop-shadow-xl"
               initial={{
-                opacity: 0
+                opacity: 0,
+                y: 50
               }}
               animate={{
-                opacity: 1
+                opacity: 1,
+                y: 0
               }}
               transition={{
                 duration: 1,
-                delay: 7.5
+                delay: delay ? delay + DELAY_SUBTITLE : DELAY_SUBTITLE,
               }}
             >
-              <Logo
-                disableHeartEnding
-                disableAnimation
-                shapeOnly
-                width={250}
-                className="absolute left-1/2 top-6 -translate-x-1/2"
-              />
+              15.11.2025
             </motion.div>
+          </div>
 
-            {/* Title */}
-            <div className="container z-10">
-              <motion.h1
-                className="text-center text-white text-9xl drop-shadow-xl overflow-hidden"
-                initial={{opacity: 0}}
-                animate={{opacity: 1}}
-                transition={{
-                  duration: 2,
-                  delay: delay ? delay + DELAY_TITLE : DELAY_TITLE,
-                }}
-              >
-                <motion.span
-                  initial={{y: 100}}
-                  animate={{y: 0}}
-                  transition={{
-                    duration: 2.5,
-                    delay: delay ? delay + DELAY_TITLE : DELAY_TITLE,
-                    ease: [0.6, 0.01, 0.05, 0.95]
-                  }}
-                  style={{display: 'inline-block'}}
-                >
-                  Save the date
-                </motion.span>
-              </motion.h1>
+          {/* Polaroids */}
+          <motion.div
+            className="absolute flex justify-center items-center w-full h-full z-0"
+            animate={{
+              opacity: 0.2
+            }}
+            transition={{
+              delay: delay ? delay + DELAY_POLAROIDS_FADE_OUT : DELAY_POLAROIDS_FADE_OUT,
+              duration: 1
+            }}
+          >
+            {[0, 1, 2].map((index) => (
               <motion.div
-                className="text-center text-white text-4xl drop-shadow-xl"
-                initial={{
-                  opacity: 0,
-                  y: 50
-                }}
-                animate={{
-                  opacity: 1,
-                  y: 0
-                }}
-                transition={{
-                  duration: 1,
-                  delay: delay ? delay + DELAY_SUBTITLE : DELAY_SUBTITLE,
-                }}
+                key={index}
+                custom={index}
+                variants={polaroidVariants}
+                initial="initial"
+                animate="animate"
+                className="absolute"
+                style={{transformOrigin: "center center"}}
               >
-                15.11.2025
+                <Polaroid priority src={`/assets/images/wedding-${index}.avif`} alt="Save the date" />
               </motion.div>
-            </div>
-
-            {/* Polaroids */}
-            <motion.div
-              className="absolute flex justify-center items-center w-full h-full z-0"
-              animate={{
-                opacity: 0.2
-              }}
-              transition={{
-                delay: delay ? delay + DELAY_POLAROIDS_FADE_OUT : DELAY_POLAROIDS_FADE_OUT,
-                duration: 1
-              }}
-            >
-              {[0, 1, 2].map((index) => (
-                <motion.div
-                  key={index}
-                  custom={index}
-                  variants={polaroidVariants}
-                  initial="initial"
-                  animate="animate"
-                  className="absolute"
-                  style={{transformOrigin: "center center"}}
-                >
-                  <Polaroid priority src={`/assets/images/wedding-${index}.avif`} alt="Save the date" />
-                </motion.div>
-              ))}
-            </motion.div>
+            ))}
           </motion.div>
-        </section>
+        </motion.section>
       )}
     </AnimatePresence>
   );
