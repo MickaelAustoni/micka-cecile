b1d47e61cc88da7680110cb6604bd51aa9b364fb
Mibiki <108873902+MickaelAustoni@users.noreply.github.com>
2024-11-29 11:46:20 +0100

    feat: improvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home({ searchParams }: { searchParams: Promise<{ name: s
   const { name } = use(searchParams);
 
   useEffect(() => {
-    setVisited(name).then();
+    void setVisited(name)
   }, [name]);
 
   return (
